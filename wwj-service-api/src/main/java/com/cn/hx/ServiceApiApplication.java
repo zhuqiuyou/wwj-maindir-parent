546e791620c0d9b6aeb03127eb05de039a46c5ec
zhuqiuyou <zhuqiuyou_zqy@163.com>
2017-11-16 11:49:41 +0800

    pom配置优化

--- wwj-service-api/src/main/java/com/cn/hx/ServiceApiApplication.java
+++ wwj-service-api/src/main/java/com/cn/hx/ServiceApiApplication.java
@@ -1,14 +1,24 @@
 package com.cn.hx;
+
+
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @MapperScan("com.cn.hx.wwj.*.*.module.*.mapper")
 @EnableTransactionManagement // 启注解事务管理，等同于xml配置方式的 <tx:annotation-driven />
 @SpringBootApplication
-public class ServiceApiApplication {
-	public static void main(String[] args) {
-		SpringApplication.run(ServiceApiApplication.class, args);
-	}
+public class ServiceApiApplication extends SpringBootServletInitializer {
+	
+	@Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(ServiceApiApplication.class);
+    }
+    
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(ServiceApiApplication.class, args);
+    }
 }
